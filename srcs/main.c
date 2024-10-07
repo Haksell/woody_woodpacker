565c84fa1c2ab99dd7dca82d1c3e360fe6a4bd3f
Axel Brisse <axel.brisse@gmail.com>
2024-10-07 05:24:02 +0200

    .

--- srcs/main.c
+++ srcs/main.c
@@ -1,18 +1,27 @@
 #include <elf.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-static void panic_syscall(const char* name) {
-    perror(name);
+void panic(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
     exit(EXIT_FAILURE);
+}
+
+static void error(const char* name) {
+    panic("woody_woodpacker: %s: %s\n", name, strerror(errno));
 }
 
 static Elf64_Ehdr* read_elf_file(const char* input_file) {
     FILE* f = fopen(input_file, "rb");
-    if (!f) panic_syscall("fopen");
+    if (!f) error("fopen");
 
     fseek(f, 0, SEEK_END);
     size_t filesize = ftell(f);
@@ -21,13 +30,13 @@ static Elf64_Ehdr* read_elf_file(const char* input_file) {
     uint8_t* buffer = malloc(filesize);
     if (!buffer) {
         fclose(f);
-        panic_syscall("malloc");
+        error("malloc");
     }
 
     if (fread(buffer, 1, filesize, f) != filesize) {
         fclose(f);
         free(buffer);
-        panic_syscall("fread");
+        error("fread");
     }
 
     fclose(f);
@@ -35,9 +44,8 @@ static Elf64_Ehdr* read_elf_file(const char* input_file) {
     Elf64_Ehdr* ehdr = (Elf64_Ehdr*)buffer;
     if (memcmp(ehdr->e_ident, ELFMAG, SELFMAG) != 0 ||
         ehdr->e_ident[EI_CLASS] != ELFCLASS64) {
-        fprintf(stderr, "File architecture not suported. x86_64 only\n");
         free(buffer);
-        exit(EXIT_FAILURE);
+        panic("File architecture not suported. x86_64 only\n");
     }
 
     return ehdr;
@@ -45,23 +53,18 @@ static Elf64_Ehdr* read_elf_file(const char* input_file) {
 
 static Elf64_Phdr* find_code_header(Elf64_Ehdr* ehdr) {
     Elf64_Phdr* phdr = (Elf64_Phdr*)((char*)ehdr + ehdr->e_phoff);
-    Elf64_Phdr* code_phdr = NULL;
     for (int i = 0; i < ehdr->e_phnum; i++) {
         if (phdr[i].p_type == PT_LOAD && (phdr[i].p_flags & PF_X)) {
-            code_phdr = &phdr[i];
-            return code_phdr;
+            return phdr + i;
         }
     }
-    fprintf(stderr, "No executable segment found\n");
     free(ehdr);
-    exit(EXIT_FAILURE);
+    panic("No executable segment found\n");
+    return NULL;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <input_binary>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+    if (argc != 2) panic("Usage: %s <input_binary>\n", argv[0]);
     Elf64_Ehdr* ehdr = read_elf_file(argv[1]);
     Elf64_Phdr* code_phdr = find_code_header(ehdr);
     printf("code_phdr->p_paddr=%zu\n", code_phdr->p_paddr);
