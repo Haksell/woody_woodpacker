78eb4b90a25ff8ab250e9c125842410090741e1d
Haksell <axel.brisse@gmail.com>
2024-10-09 03:36:06 +0200

    dodo

---
+++ des.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char input[] = "je mappelle axel et jaime le chocolat";
+    size_t n = strlen(input);
+    printf("%s %zu\n", input, n);
+}
