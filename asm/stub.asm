ab0fada7b4e8deccd366b5c1789374a5c0a6be19
Obito-git <antonmyroshnychenko@gmail.com>
2024-10-10 00:10:56 +0200

    generate machine code from asm directly

---
+++ asm/stub.asm
@@ -0,0 +1,28 @@
+BITS 64
+global main
+
+main:
+    jmp get_string
+
+print:
+    mov rax, 0x1
+    mov rdi, 0x1
+    pop rsi
+    mov rdx, 0xe
+    syscall
+    jmp end
+
+get_string:
+    call print
+    db `....WOODY....\n`
+
+end:
+    xor rax, rax
+    xor rdi, rdi
+    xor rdx, rdx
+    xor rsi, rsi
+
+    jmp [rel jump_placeholder]
+
+jump_placeholder:
+    dq 0xFFFFFFFFFFFFFF
